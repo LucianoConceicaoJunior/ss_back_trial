dec53c9bdc00f385dc2b5f5898fd4cf585967895
Luciano Junior <lucianodcjr@gmail.com>
2023-07-18 00:30:35 -0300

    Added Rspec and some adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :rentals
   has_many :favorites, through: :favorite_movies, source: :movie
   has_many :rented, through: :rentals, source: :movie
+
+  before_create :generate_api_key
+
+  def generate_api_key
+    self.api_key = SecureRandom.urlsafe_base64
+  end
 end
